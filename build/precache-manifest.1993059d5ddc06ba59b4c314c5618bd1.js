ef3b54f85d5fce9aa2764f2b4e080304ffbf5a33
Balraj Singh <balraj.singh@zemosolabs.com>
2019-07-02 17:49:54 +0530

    Add prod build

---
+++ build/precache-manifest.1993059d5ddc06ba59b4c314c5618bd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c3e4f0c55253da29b22a32cc139aa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "722f9bb6d674c0682a2c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "722f9bb6d674c0682a2c",
+    "url": "/static/js/main.233ae7e2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
